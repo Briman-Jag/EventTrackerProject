a0807e650ab39444bc51160a36f1ca8d628e3372
Briman-Jag <brianstreetman25@gmail.com>
2019-11-30 20:54:28 -0700

    Uncommented tomcat stuff in application.properties

--- EventTracker/src/test/java/com/skilldistillery/eventtracker/controllers/EventTrackerWebIntegrationTest.java
+++ EventTracker/src/test/java/com/skilldistillery/eventtracker/controllers/EventTrackerWebIntegrationTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 import java.io.IOException;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
