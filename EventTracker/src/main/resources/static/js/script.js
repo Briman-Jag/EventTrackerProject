22ef57570d1cf6fab300188d7fbd2b4043c5d18d
Briman-Jag <brianstreetman25@gmail.com>
2019-12-08 18:21:08 -0700

    Finished List All Music Festival function and created borders on table

--- EventTracker/src/main/resources/static/js/script.js
+++ EventTracker/src/main/resources/static/js/script.js
@@ -1,43 +1,53 @@
 window.addEventListener('load', function(e) {
 	console.log("Document Loaded");
 	init();
-	
+
 });
 
-function init(){
-	// TODO  Add event listeners to buttons; Call functions to put events on table;
+function init() {
+	// TODO Add event listeners to buttons; Call functions to put events on
+	// table;
 	document.musicFestForm.lookup.addEventListener('click', function(event) {
-	    event.preventDefault();
-	    var musicFestId = document.musicFestForm.musicFestId.value;
-	    if (!isNaN(musicFestId) && musicFestId > 0) {
-	      getMusicFest(musicFestId);
-	    }
-	  });
-	
-	  document.addMusicFestForm.addMusicFest.addEventListener('click', function(event) {
-		    event.preventDefault();
-		    addMusicFest();
-		  });
+		event.preventDefault();
+		var musicFestId = document.musicFestForm.musicFestId.value;
+		if (!isNaN(musicFestId) && musicFestId > 0) {
+			getMusicFest(musicFestId);
+		}
+	});
+
+	document.listAllMusicFests.listAll.addEventListener('click',
+			function(event) {
+				event.preventDefault();
+				listAllMusicFests();
+
+			});
+
+	document.addMusicFestForm.addMusicFest.addEventListener('click', function(
+			event) {
+		event.preventDefault();
+		addMusicFest();
+	});
 }
 
 // Function to get MusicFestival
 
-function getMusicFest(musicFestId){
+function getMusicFest(musicFestId) {
 
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://localhost:8087/api/musicfestivals/' + musicFestId, true);
+	xhr.open('GET', 'http://localhost:8087/api/musicfestivals/' + musicFestId,
+			true);
 
 	xhr.onreadystatechange = function() {
 
-	  console.log(xhr.readyState);
-	  console.log(xhr.status);
+		console.log(xhr.readyState);
+		console.log(xhr.status);
 
 		if (xhr.readyState === 4 && xhr.status < 400) {
 			var musicFest = JSON.parse(xhr.responseText);
 			console.log(musicFest);
 			displayMusicFest(musicFest);
 
-	  }
+		}
 		if (xhr.readyState === 4 && xhr.status >= 400) {
 			// ToDo: Display an error on web page.
 			console.error(xhr.status + ': ' + xhr.responseText);
@@ -49,56 +59,58 @@ function getMusicFest(musicFestId){
 }
 
 function displayMusicFest(musicFest) {
-	
-	  var dataDiv = document.getElementById('musicFestData');
-	  dataDiv.textContent = '';
-	  while (dataDiv.firstElementChild) {
-			dataDiv.removeChild(dataDiv.firstElementChild);
-		}
-	  
-	  // Music Festival Name Header
-	  let h1 = document.createElement('h1');
-	  dataDiv.appendChild(h1);
-	  h1.textContent = musicFest.name;
-	  
-	 // Music Festival Details Table
+
+	var dataDiv = document.getElementById('musicFestData');
+	dataDiv.textContent = '';
+	while (dataDiv.firstElementChild) {
+		dataDiv.removeChild(dataDiv.firstElementChild);
+	}
+
+	// Music Festival Name Header
+	let h1 = document.createElement('h1');
+	dataDiv.appendChild(h1);
+	h1.textContent = musicFest.name;
+
+	// Music Festival Details Table
 	// Table Creation Functions for Cells and rows
-	  var tbl = document.createElement('table');
-	  var tbdy = document.createElement('tbody');
-	  
-	  tbl.appendChild(tbdy);
-  	
-	    addRow('Location:', musicFest.location);
-	    addRow('Number of Days:', musicFest.numOfDays);
-	    addRow('Start Date:', musicFest.startDate);
-	    addRow('End Date:', musicFest.endDate);
-	    addRow('Music Genre(s):', musicFest.musicGenre);
-	    addRow('Headliners:', musicFest.headliners);
-	    addRow('Artists Seen:', musicFest.artistsSeen);
-	    addRow('Ticket Price:', '$ ' + musicFest.ticketPrice);
-	  
-	    dataDiv.appendChild(tbl);
-	    
-	  function addCell(tr, val) {
-	      var td = document.createElement('td');
+	var tbl = document.createElement('table');
+	tbl.style.border = "solid";
+	var tbdy = document.createElement('tbody');
+	tbdy.style.border = "solid";
+	
+	tbl.appendChild(tbdy);
 
-	      td.innerHTML = val;
+	addRow('Location:', musicFest.location);
+	addRow('Number of Days:', musicFest.numOfDays);
+	addRow('Start Date:', musicFest.startDate);
+	addRow('End Date:', musicFest.endDate);
+	addRow('Music Genre(s):', musicFest.musicGenre);
+	addRow('Headliners:', musicFest.headliners);
+	addRow('Artists Seen:', musicFest.artistsSeen);
+	addRow('Ticket Price:', '$ ' + musicFest.ticketPrice);
 
-	      tr.appendChild(td)
-	    }
+	dataDiv.appendChild(tbl);
 
-	    function addRow(val_1, val_2) {
-	      var tr = document.createElement('tr');
+	function addCell(tr, val) {
+		var td = document.createElement('td');
 
-	      addCell(tr, val_1);
-	      addCell(tr, val_2);
+		td.innerHTML = val;
 
-	      tbdy.appendChild(tr);
-	    }
-	  
+		tr.appendChild(td)
+	}
+
+	function addRow(val_1, val_2) {
+		var tr = document.createElement('tr');
+
+		addCell(tr, val_1);
+		addCell(tr, val_2);
+
+		tbdy.appendChild(tr);
+	}
+
 }
 
-function addMusicFest(){
+function addMusicFest() {
 	console.log('Adding new Music Festival!');
 	// Add Post xhr code
 	var xhr = new XMLHttpRequest();
@@ -115,7 +127,7 @@ function addMusicFest(){
 			dataDiv.textContent = 'Error Adding Music Festival';
 		}
 	};
-	
+
 	let form = document.addMusicFestForm;
 	var newMusicFestObject = {
 		name : form.name.value,
@@ -127,14 +139,120 @@ function addMusicFest(){
 		headliners : form.headliners.value,
 		artistsSeen : form.artistsSeen.value,
 		ticketPrice : form.ticketPrice.value
-		
+
 	};
 	// Convert JS object to JSON string
-	var newMusicFestJsonString = JSON.stringify(newMusicFestObject); 
+	var newMusicFestJsonString = JSON.stringify(newMusicFestObject);
 	xhr.send(newMusicFestJsonString);
 }
 
+function listAllMusicFests() {
 
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://localhost:8087/api/musicfestivals', true);
 
+	xhr.onreadystatechange = function() {
 
+		console.log(xhr.readyState);
+		console.log(xhr.status);
 
+		if (xhr.readyState === 4 && xhr.status < 400) {
+			var musicFests = JSON.parse(xhr.responseText);
+			console.log(musicFests);
+			displayAllMusicFests(musicFests);
+		}
+		if (xhr.readyState === 4 && xhr.status >= 400) {
+			// ToDo: Display an error on web page.
+			console.error(xhr.status + ': ' + xhr.responseText);
+			var dataDiv = document.getElementById('musicFestData');
+			dataDiv.textContent = ' No Music Festivals Found!';
+		}
+	};
+	xhr.send(null);
+}
+
+function displayAllMusicFests(musicFests) {
+
+	var dataDiv = document.getElementById('musicFestData');
+	dataDiv.textContent = '';
+	while (dataDiv.firstElementChild) {
+		dataDiv.removeChild(dataDiv.firstElementChild);
+	}
+
+	// Music Festival Name Header
+	let h1 = document.createElement('h1');
+	dataDiv.appendChild(h1);
+	h1.textContent = 'Music Festival List';
+	
+	let h3 = document.createElement('h3');
+	dataDiv.appendChild(h3);
+	h3.textContent = '(Click Row to Edit)'; 
+
+	// Music Festival Details Table
+	// Table Creation Functions for Cells and rows
+	var tbl = document.createElement('table');
+	tbl.style.border = "solid";
+	var tbdy = document.createElement('tbody');
+	tbdy.style.border = "solid";
+
+	// Body fields of table
+	tbl.appendChild(tbdy);
+
+	// Table Header function
+	function addHeader(name) {
+		var th = document.createElement('th');
+		th.style.border = "solid";
+		th.innerHTML = name;
+		tbdy.appendChild(th);
+	}
+
+	// Table Headers
+	addHeader('Name');
+	addHeader('Location');
+	addHeader('Number of Days');
+	addHeader('Start Date');
+	addHeader('End Date');
+	addHeader('Music Genre(s)');
+	addHeader('Headliners');
+	addHeader('Artists Seen');
+	addHeader('Ticket Price');
+
+	for (let i = 0; i < musicFests.length; i++) {
+		//Click Event for updates
+		// Table Rows
+		addRow(musicFests[i].name, musicFests[i].location,
+				musicFests[i].numOfDays, musicFests[i].startDate,
+				musicFests[i].endDate, musicFests[i].musicGenre,
+				musicFests[i].headliners, musicFests[i].artistsSeen, 
+				'$ ' + musicFests[i].ticketPrice);
+
+		dataDiv.appendChild(tbl);
+	}
+	// Add cell function
+	function addCell(tr, val) {
+		var td = document.createElement('td');
+		td.style.border = "solid";
+
+		td.innerHTML = val;
+
+		tr.appendChild(td)
+	}
+	// Add row function
+	function addRow(val_1, val_2, val_3, val_4, val_5, val_6, val_7, val_8,
+			val_9) {
+		var tr = document.createElement('tr');
+		tr.style.border = "solid";
+
+		addCell(tr, val_1);
+		addCell(tr, val_2);
+		addCell(tr, val_3);
+		addCell(tr, val_4);
+		addCell(tr, val_5);
+		addCell(tr, val_6);
+		addCell(tr, val_7);
+		addCell(tr, val_8);
+		addCell(tr, val_9);
+		tbdy.appendChild(tr);
+	}
+
+}
